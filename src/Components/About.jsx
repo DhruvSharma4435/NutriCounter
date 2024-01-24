bc35423ebdaf11c38a4b860dae0423688301b1c2
DhruvSharma4435 <dhruvsharma9454@gmail.com>
2024-01-24 23:17:40 +0530

    first commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ChartLogo from '../Assets/ChartLogo.png'
+
+export default function About() {
+    return (
+        <>
+            <div className="container d-flex justify-content-center align-items-center flex-column">
+                <img style={{ height: "auto", width: '100%', maxHeight: '450px', maxWidth: '450px' }} src={ChartLogo} alt="logo" />
+                <div className="my-1">
+                    <h1 className='py-2 card-title'>What is Nutricounter?</h1>
+                    <p className='card-text'>An website which compare two fruits Nutrition Data in Different type of Charts.</p>
+                    <p className='card-text'>Nutrition, storage and handling information on a variety of fruits and vegetables. One look and you’ll see that eating a colorful variety of fruits and veggies provides a wide range of valuable nutrients.</p>
+                  <a  href='https://drive.google.com/file/d/1To23qckyNRMCZs7LmBEl_sSVo5FTgsDC/view?pli=1'>  <button type='button' className='btn btn-info'> My Resume</button> </a>
+                </div>
+            </div>
+        </>
+    )
+}
